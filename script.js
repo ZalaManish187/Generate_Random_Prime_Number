303c6ef0644510505d446d3331220dbb96382e2b
Manish Zala <zalamanish187@gmail.com>
2025-08-07 13:04:07 +0530

    07-08-2025

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const primeGen = document.getElementById("prime");
+const primeJoin = document.getElementById("primeJoin");
 
 function primeNum() {
   let start = document.getElementById("start").value;
   let second = document.getElementById("second").value;
-  let length = document.getElementById("count").value;
+  let length = 5;
+
   let number = [];
-  for (let i = start; i <= second; i++) {
+  for (let i = start; i < second; i++) {
     let isPrime = true;
     for (let j = 2; j < i; j++) {
       if (i % j === 0) {
@@ -19,10 +21,14 @@ function primeNum() {
   }
 
   let randomPrime = [];
-  while (length > randomPrime.length) {
-    randomPrime.push(number[Math.floor(Math.random() * number.length)]);
+  while (randomPrime.length < length) {
+    let randomIndex = Math.floor(Math.random() * number.length);
+
+    randomPrime.push(number[randomIndex]);
   }
-  primeGen.value = randomPrime.join(" ");
+
+  primeGen.value = randomPrime;
+  primeJoin.value = randomPrime.join("");
 }
 
 primeNum();
